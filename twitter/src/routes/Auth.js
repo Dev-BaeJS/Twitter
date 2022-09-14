7923a3997183d83cbd1429df850a7549d81114c8
배정수 <97235836+Dev-BaeJS@users.noreply.github.com>
2022-09-15 01:41:35 +0900

    Firebase Setting and Login Function

--- twitter/src/routes/Auth.js
+++ twitter/src/routes/Auth.js
@@ -1,3 +1,51 @@
-const Auth = () => <span>Auth</span>;
+import { useState } from "react";
+
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChange = (event) =>{
+        const {
+            target: {name, value},
+        } = event;
+        if(name == "email"){
+            setEmail(value);
+        } else if(name == "password"){
+            setPassword(value);
+        }
+    }
+
+    const onSubmit = (event) =>{
+        event.prventDefault();
+    }
+
+    return(
+        <div>
+            <form onSubmit={onSubmit}>
+                <input 
+                    name="email"
+                    type="email" 
+                    placeholder="Email" 
+                    value={email}
+                    onChange={onChange}
+                    required 
+                    />
+                <input 
+                    name="password"
+                    type="password" 
+                    placeholder="Password"
+                    value={password}
+                    onChange={onChange}
+                    required 
+                    />
+                <input type="submit" value="Log In" />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    );
+};
 
 export default Auth;
