4e4c6d6fed4ad307f6040f9992022c24391ed6ac
배정수 <97235836+Dev-BaeJS@users.noreply.github.com>
2022-09-15 02:19:09 +0900

    Use Auth Email, Password Function

--- twitter/src/routes/Auth.js
+++ twitter/src/routes/Auth.js
@@ -1,23 +1,50 @@
+import { authService } from "fbase";
 import { useState } from "react";
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+  } from "firebase/auth";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
 
     const onChange = (event) =>{
         const {
             target: {name, value},
         } = event;
-        if(name == "email"){
+        if(name === "email"){
             setEmail(value);
-        } else if(name == "password"){
+        } else if(name === "password"){
             setPassword(value);
         }
-    }
+    };
 
-    const onSubmit = (event) =>{
-        event.prventDefault();
-    }
+    const onSubmit = async (event) =>{
+        event.preventDefault();
+        try {
+            let data;
+            if(newAccount){
+                //create newAccount
+                data = await createUserWithEmailAndPassword(
+                    authService,
+                    email, 
+                    password
+                    );
+            } else{
+                // log in
+                data = await signInWithEmailAndPassword(
+                    authService,
+                    email, 
+                    password
+                    );
+            }
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return(
         <div>
@@ -38,7 +65,7 @@ const Auth = () => {
                     onChange={onChange}
                     required 
                     />
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
             </form>
             <div>
                 <button>Continue with Google</button>
