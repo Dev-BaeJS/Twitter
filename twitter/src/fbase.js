b179cff777cab2240ba97cecb48dea89166b0629
배정수 <97235836+Dev-BaeJS@users.noreply.github.com>
2022-09-16 01:21:28 +0900

    Add Navigation, Logout

--- twitter/src/fbase.js
+++ twitter/src/fbase.js
@@ -16,3 +16,6 @@ export const SignInWithPopup = signInWithPopup;
 export const googleAuthProvider = GoogleAuthProvider;
 export const githubAuthProvider = GithubAuthProvider;
 export const authService = getAuth();
+
+// "firebase" -v === 
+// https://velog.io/@seondal/Firebase-v9%EB%B6%80%ED%84%B0-%EB%8B%AC%EB%9D%BC%EC%A7%84-%EC%9D%B8%EC%A6%9D%EB%AA%A8%EB%93%88-%EC%82%AC%EC%9A%A9%EB%B2%95
